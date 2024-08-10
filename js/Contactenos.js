d585254fea425df5deed2b14c08ebf9d2c371eb0
Jacksondg712 <118758501+Jacksondg712@users.noreply.github.com>
2024-08-09 21:05:06 -0400

    emailjs

--- js/Contactenos.js
+++ js/Contactenos.js
@@ -1,17 +1,3 @@
-// document.getElementById('Contact_Form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-    
-//     const serviceID = 'service_mcmyb2j';
-//     const templateID = 'template_peqxgcb';
-
-//     emailjs.sendForm(serviceID, templateID, this)
-//         .then(() => {
-//             alert('Mensaje enviado!');
-//         }, (err) => {
-//             alert(JSON.stringify(err));
-//         });
-// });
-
 const btn = document.getElementById('button');
 
 document.getElementById('form')
