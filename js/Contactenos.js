ac6b3c040dd44eec88a1840e76d0dc050b35e5c7
Jacksondg712 <118758501+Jacksondg712@users.noreply.github.com>
2025-10-06 02:51:18 -0400

    incompleto responsive
    quede en "trabajo" especificamente en la adecuacion del formulario en 480px

--- js/Contactenos.js
+++ js/Contactenos.js
@@ -3,6 +3,38 @@ var modal = document.getElementById("myModal");
 var bton = document.getElementById("openModalBtn");
 var span = document.getElementsByClassName("close")[0];
 
+// reponsive scroll de hamburguesa 
+const menuToggle = document.getElementById('menuToggle');
+const navWrapper = document.getElementById('navWrapper');
+
+// Hrader responsive 
+
+menuToggle.addEventListener('click', function() {
+            this.classList.toggle('active');
+            navWrapper.classList.toggle('active');
+        });
+
+        // Cerrar menú al hacer clic en un enlace
+        const menuLinks = document.querySelectorAll('.nav-wrapper a');
+        menuLinks.forEach(link => {
+            link.addEventListener('click', function() {
+                menuToggle.classList.remove('active');
+                navWrapper.classList.remove('active');
+            });
+        });
+
+        // Cerrar menú al hacer clic fuera
+        document.addEventListener('click', function(event) {
+            const isClickInsideMenu = navWrapper.contains(event.target);
+            const isClickOnToggle = menuToggle.contains(event.target);
+            
+            if (!isClickInsideMenu && !isClickOnToggle && navWrapper.classList.contains('active')) {
+                menuToggle.classList.remove('active');
+                navWrapper.classList.remove('active');
+        }
+});
+
+
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
